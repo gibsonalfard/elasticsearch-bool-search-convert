b4e0803cdc8a45bacd3afeeb8b03a7abdd682cb4
Ilham Gibran <ilgibrz@gmail.com>
2020-11-12 10:40:22 +0700

    Change config

--- elasticsearch-news-api/elasticsearch/config.js
+++ elasticsearch-news-api/elasticsearch/config.js
@@ -1,4 +1,4 @@
 const { Client } = require('@elastic/elasticsearch')
-const esClient = new Client({ node: 'http://203.210.84.102:9210' })
+const esClient = new Client({ node: 'http://localhost:9200' })
 
 module.exports = esClient;
